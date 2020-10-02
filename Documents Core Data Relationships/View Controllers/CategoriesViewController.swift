28bf4311136c675b43f30c4969568360c583d86b
Melissa Crozier <mgckq8@mail.missouri.edu>
2020-10-02 13:53:02 -0500

    Set up storyboard

---
+++ Documents Core Data Relationships/View Controllers/CategoriesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoriesViewController.swift
+//  Documents Core Data Relationships
+//
+//  Created by Melissa Crozier on 10/2/20.
+//  Copyright © 2020 Melissa Crozier. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Documents Core Data Relationships/View Controllers/NewDocumentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewDocumentViewController.swift
+//  Documents Core Data Relationships
+//
+//  Created by Melissa Crozier on 10/2/20.
+//  Copyright © 2020 Melissa Crozier. All rights reserved.
+//
+
+import UIKit
+
+class NewDocumentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
